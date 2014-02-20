f9423de2654bba22c1e6709b9a43f80a0edacc9f
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2014-02-20 15:25:40 -0300

    Adicionado bind no IE8 e corrigido o addEventListener no IE8 pra passar contexto do elemento

--- draughts.js
+++ draughts.js
@@ -1,5 +1,29 @@
 /*global CustomEvent, MouseEvent, HTMLDocument*/
 (function () {
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function (oThis) {
+            if (typeof this !== "function") {
+                // closest thing possible to the ECMAScript 5 internal IsCallable function
+                throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+            }
+            
+            var aArgs = Array.prototype.slice.call(arguments, 1), 
+                fToBind = this, 
+                fNOP = function () {},
+                fBound = function () {
+                    return fToBind.apply(this instanceof fNOP && oThis
+                            ? this
+                            : oThis,
+                            aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+            
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+            
+            return fBound;
+        };
+    }
+
     if (Element.prototype.addEventListener || !Object.defineProperty) {
         return;
     }
@@ -33,7 +57,7 @@
             return;
         }
     
-        this.attachEvent('on' + type, fn);
+        this.attachEvent('on' + type, fn.bind(this));
     };
     
     // setup the DOM and window objects
